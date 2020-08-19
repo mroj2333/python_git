e6544289901f6894a4e29509a18cb5bf601ca1b1
时光与你Mroj <mroj2333@sina.com>
2020-08-19 18:31:31 +0800

    -

---
+++ djang_vue/10-axios_ajax/bookmanager/book/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+# Create your views here.
+
+
+class LoginView(View):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
